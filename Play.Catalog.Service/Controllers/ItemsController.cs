539c6c98f1dc4847c2c99146af3ab6fe7d8f03e0
nlights <elhassanianas@gmail.com>
2023-03-04 16:09:52 +0100

    Mongodb+Controllers update

--- Play.Catalog.Service/Controllers/ItemsController.cs
+++ Play.Catalog.Service/Controllers/ItemsController.cs
@@ -1,8 +1,11 @@
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using Play.Catalog.Service.Dtos;
+using Play.Catalog.Service.Repositories;
 using System;
 using System.Linq;
+using System.Threading.Tasks;
+using Play.Catalog.Service.Entities;
 
 namespace Play.Catalog.Service.Controllers
 {
@@ -12,12 +15,14 @@ namespace Play.Catalog.Service.Controllers
     public class ItemsController : ControllerBase
     {
 
-        private static readonly List<ItemDto> items = new()
+        /*private static readonly List<ItemDto> items = new()
         {
             new ItemDto(Guid.NewGuid(),"Potion A","Potion A description", 7, System.DateTimeOffset.UtcNow),
             new ItemDto(Guid.NewGuid(),"Potion B","Potion B description", 17, System.DateTimeOffset.UtcNow),
             new ItemDto(Guid.NewGuid(),"Potion C","Potion C description", 27, System.DateTimeOffset.UtcNow)
-        };
+        };*/
+
+        private readonly ItemsRepository itemsRepository = new();
 
         [HttpGet]
         [Route("/")]
@@ -28,74 +33,78 @@ namespace Play.Catalog.Service.Controllers
 
 
         [HttpGet]
-        public ActionResult<List<ItemDto>> GetList()
+        public async Task<IEnumerable<ItemDto>> GetListAsync()
         {
-            return Ok(items);
+            var items = (await itemsRepository.GetAllAsync()).Select(item => item.AsDto());
+            return items;
         }
 
         [HttpGet]
         [Route("{id}")]
-        public ActionResult<ItemDto> GetItem(Guid id)
+        public async Task<ActionResult<ItemDto>> GetItemAsync(Guid id)
         {
             //var item = items.Find(i => i.Id == id);
-            var item = items.Where(i => i.Id == id).SingleOrDefault();
+            //var item = items.Where(i => i.Id == id).SingleOrDefault();
+            var item = await itemsRepository.GetAsync(id);
+
             if (item == null)
             {
                 return NotFound("Item not found !!");
             }
-            return Ok(item);
+            return item.AsDto();
         }
 
         [HttpDelete]
         [Route("{id}")]
-        public ActionResult<ItemDto> DeleteItem(Guid id)
+        public async Task<ActionResult> DeleteItemAsync(Guid id)
         {
-            //var item = items.Find(i => i.Id == id);
-            //var item = items.Where(i => i.Id == id).SingleOrDefault();
+            var item = await itemsRepository.GetAsync(id);
+            if (item == null)
+            {
+                return NotFound("Item not found !!");
+            }
 
-            var index = items.FindIndex(i => i.Id == id);
-             if (index < 0)
-             {
-                 return NotFound("Item not found !!");
-             }
-
-            items.RemoveAt(index);
-            return Ok(items);
+            await itemsRepository.DeleteAsync(id);
+            return NoContent();
         }
 
 
-       [HttpPut]
-         [Route("{id}")]
-         public ActionResult<ItemDto> UpdateItem(Guid id, UpdateItemDto updatedItemDto)
-         {
-             //var item = items.Find(i => i.Id == id);
-             var existingItem = items.Where(i => i.Id == id).SingleOrDefault();
-             
+        [HttpPut]
+        [Route("{id}")]
+        public async Task<ActionResult> UpdateItem(Guid id, UpdateItemDto updatedItemDto)
+        {
+            //var item = items.Find(i => i.Id == id);
+            var existingItem = await itemsRepository.GetAsync(id);
 
-             if (existingItem == null)
-             {
-                 return NotFound("Item not found !!");
-             }
 
-             var updatedItem = existingItem with {
-                Name = updatedItemDto.Name,
-                Description = updatedItemDto.Description,
-                Price = updatedItemDto.Price
-             };
+            if (existingItem == null)
+            {
+                return NotFound("Item not found !!");
+            }
+
+            existingItem.Name = updatedItemDto.Name;
+            existingItem.Description = updatedItemDto.Description;
+            existingItem.Price = updatedItemDto.Price;
 
             // var index = items.IndexOf(existingItem);
-            var index = items.FindIndex(existingItem =>existingItem.Id == id);
-            items[index] = updatedItem;
-             return Ok(updatedItem);
-         }
- 
+            //var index = items.FindIndex(existingItem => existingItem.Id == id);
+            //items[index] = updatedItem;
+
+            await itemsRepository.UpdateAsync(existingItem);
+            return NoContent();
+            //return Ok(updatedItem);
+        }
+
         [HttpPost]
-        public ActionResult<ItemDto> PostItem(CreateItemDto newItem)
+        public async Task<ActionResult<ItemDto>> PostItem(CreateItemDto newItem)
         {
 
-            var NewItemDto = new ItemDto(Guid.NewGuid(), newItem.Name, newItem.Description, newItem.Price, DateTimeOffset.UtcNow);
-            items.Add(NewItemDto);
-            return Ok("Items created successfully !!");
+            var item = new Item { Name = newItem.Name, Description = newItem.Description, Price = newItem.Price, CreatedAt = DateTimeOffset.UtcNow };
+            //var NewItemDto = new ItemDto(Guid.NewGuid(), newItem.Name, newItem.Description, newItem.Price, DateTimeOffset.UtcNow);
+            await itemsRepository.CreateAsync(item);
+            // ID format issue
+            //return CreatedAtAction(nameof(GetItemAsync), new { id = item.Id }, item);
+            return item.AsDto();
         }
 
     }
--- Play.Catalog.Service/Dtos.cs
+++ Play.Catalog.Service/Dtos.cs
@@ -6,7 +6,7 @@ namespace Play.Catalog.Service.Dtos
 {
     public record ItemDto(Guid Id, string Name, string Description, decimal Price, DateTimeOffset CreatedAt);
 
-    public record CreateItemDto([Required] string Name, string Description, [Range(0, 1000)]  decimal Price);
+    public record CreateItemDto([Required] string Name, string Description, [Range(0, 1000)] decimal Price);
 
-    public record UpdateItemDto([Required] string Name, string Description, [Range(0, 1000)]  decimal Price);
+    public record UpdateItemDto([Required] string Name, string Description, [Range(0, 1000)] decimal Price);
 }
